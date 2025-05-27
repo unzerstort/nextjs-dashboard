a01586f3cab3134ce012e5a5fe9a4c3053e1356a
Sandra Bastos <sandra.bastos@alura.com.br>
2025-05-27 17:26:10 -0300

    adds missing import

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,7 @@
 
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
