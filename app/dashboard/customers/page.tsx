be12334ba7bb662eb0cb25812d0e935e67471aef
Sandra Bastos <sandra.bastos@alura.com.br>
2025-05-28 18:33:33 -0300

    adds customer table on customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,39 @@
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
+import CustomersTable from '@/app/ui/customers/table';
+import Search from '@/app/ui/search';
 import { Metadata } from 'next';
+import Pagination from '@/app/ui/invoices/pagination';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 
 export const metadata: Metadata = {
   title: 'Customers Page',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const customers = await fetchFilteredCustomers(query);
+  const totalPages = await fetchCustomers(query);
+
+  return (
+    <div className="w-full">
+
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
